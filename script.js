dacd71ae08ff86a25ec879ae5c75dc42888fc5ac
swirly-way <s4m14z1l@gmail.com>
2024-05-17 22:30:35 +0100

    Add same class to all panels for same style and an event listener that changes color when cursor is on one of the panels

--- script.js
+++ script.js
@@ -1,19 +1,16 @@
 const container = document.getElementById('container');
 
-const panelNum = 16;
+const panelNum = 64;
 
 
 function addSquare() {
     for(let i = 1; i <= panelNum; i++){
         const panel = document.createElement("div");
-        
-        panel.style.height = '100px';
-        panel.style.width = '100px';
-        panel.style.backgroundColor = 'green';
-
-      
-        // add the newly created element and its content into the DOM
+        panel.classList.add('panel');
         container.appendChild(panel);
+        panel.addEventListener('mouseover', function(){
+            panel.style.backgroundColor = 'white';
+        })
     };
 };
 addSquare();
