a4c8384c1045dcda574d4c5f09d4ee5b03d64563
swirly-way <s4m14z1l@gmail.com>
2024-05-17 21:25:50 +0100

    Rename variable for better readability and create a function that creates square divs and appends them to the container

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 const container = document.getElementById('container');
 
-const numOfSquares = 16;
+const panelNum = 16;
 
 
+function addSquare() {
+    for(let i = 1; i <= panelNum; i++){
+        const panel = document.createElement("div");
+        
+        panel.style.height = '100px';
+        panel.style.width = '100px';
+        panel.style.backgroundColor = 'green';
+
+      
+        // add the newly created element and its content into the DOM
+        container.appendChild(panel);
+    };
+};
+addSquare();
