4dbc4dbd918989ce94894dbf9db5ba123e19dc9d
swirly-way <s4m14z1l@gmail.com>
2024-05-22 20:12:11 +0100

    Declare a variable which will determine wether random mode for colors is on or off

--- script.js
+++ script.js
@@ -3,19 +3,19 @@ const container = document.getElementById('container');
 let input = prompt('How many squares per side?');
 let inputInt = Number(input);
 let panelNum =  inputInt * inputInt;
-
+let randomOn = false;
 
 function generateScreen() {
 
    function gridGenerator(){
     for(let i = 1; i <= panelNum; i++){
         const panel = document.createElement("div");
+        panel.addEventListener('mouseover', function(){
+            panel.style.backgroundColor = 'black';
+            });
         panel.classList.add('panel');
         container.appendChild(panel);
         panel.style.flexBasis = `calc(100% / ${inputInt})`;
-        panel.addEventListener('mouseover', function(){
-        panel.style.backgroundColor = 'black';
-        });
     };
 }
 gridGenerator();
